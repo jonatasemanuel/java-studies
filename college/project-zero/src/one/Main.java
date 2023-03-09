77780491030e410dc958c6e452b46144fd2e3f27
Jonatas E S Silva <jonatassilvaemanuel@gmail.com>
2023-03-08 21:49:46 -0300

    imp: python to java

---
+++ college/project-zero/src/one/Main.java
@@ -0,0 +1,31 @@
+package one;
+
+public class Main {
+    public static void main(String[] args) {
+        // Variables
+        int age; // 4bytes
+        float weight; // 4bytes
+
+        // double -> precision values
+        double velocity; // 8bytes
+
+        char initial = 'L';
+        boolean ofAge = true;
+        String name ="Mario";
+
+        int lengthString = name.length();
+        System.out.println("The string has:" + lengthString + " characters");
+
+        // Arithmetical operation
+        int result;
+        result = 4 + 1;
+
+        System.out.println(result);
+        
+
+    }
+}
+
+
+// " " -> string
+// ' ' -> char
